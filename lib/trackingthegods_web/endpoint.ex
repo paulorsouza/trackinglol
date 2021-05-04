23ce7cdfcd6e483580a8ed5acf50b4d7502a0ef6
paulorsouza <paulor1809@gmail.com>
2021-05-04 17:00:16 -0300

    Create riot.txt

--- lib/trackingthegods_web/endpoint.ex
+++ lib/trackingthegods_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule TrackingthegodsWeb.Endpoint do
     at: "/",
     from: :trackingthegods,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt riot.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
